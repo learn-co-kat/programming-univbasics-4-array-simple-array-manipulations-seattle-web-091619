1a52abefa1377b42ee2f4e7692f58850bef980dc
Katrina R <katrina.reinsdorff@gmail.com>
2019-08-15 20:12:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -34,8 +34,8 @@ def using_uniq (array)
   array.uniq 
 end 
 
-def using_flatten (array)
-  array.flatten
+def using_flatten (instruments)
+  instruments.flatten
 end 
 
 def using_flatten (array, string)
